36489683bc64e211f89a832241e2285540a4f452
Jeremy Stephens <viking415@gmail.com>
2010-01-17 16:03:53 -0600

    GTK autotools config

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
-#include "database.h"
+#include "main.h"
 
 int
 main(argc, argv)
   int argc;
   char **argv;
 {
+  GtkWidget *window;
+  GtkWidget *button;
+
+  gtk_init(&argc, &argv);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   return 0;
 }
